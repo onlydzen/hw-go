9cab71a0cb72e4e6b090b01630f6457ea0e58418
onlydzen <dzen898@yandex.ru>
2021-12-07 19:24:33 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,162 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+const backslash rune = 92
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+var (
+	ErrInvalidStringDigitFirst          = errors.New("invalid string – first symbol can't be a digit")
+	ErrInvalidStringDigitInRawCount     = errors.New("invalid string – can't have two or more digits in a row")
+	ErrInvalidStringBackslashSequence   = errors.New("invalid string – after backslash can stand only digit or \\")
+	ErrInvalidStringBackslashInRawCount = errors.New("invalid string – max two backslashes in a raw are valid")
+)
+
+func Unpack(data string) (string, error) {
+	dataRunes := []rune(data)
+
+	dataRunesLen := len(dataRunes)
+	if dataRunesLen == 0 {
+		return "", nil
+	}
+
+	dataIsValid, err := isDataValid(dataRunes)
+	if !dataIsValid {
+		return "", err
+	}
+
+	builder := strings.Builder{}
+	isPreviousStepRepeated := false
+
+	for i := 0; i < dataRunesLen; i++ {
+		if i == 0 {
+			processRune(&builder, 0, dataRunes[i], dataRunes[i+1], &isPreviousStepRepeated)
+			continue
+		}
+
+		if i == dataRunesLen-1 {
+			processRune(&builder, dataRunes[i-1], dataRunes[i], 0, &isPreviousStepRepeated)
+			break
+		}
+
+		processRune(&builder, dataRunes[i-1], dataRunes[i], dataRunes[i+1], &isPreviousStepRepeated)
+	}
+
+	return builder.String(), nil
+}
+
+func processRune(b *strings.Builder, previousRune rune, currentRune rune, nextRune rune, isPreviousStepRepeated *bool) {
+	if (currentRune == backslash || unicode.IsDigit(currentRune)) && previousRune != backslash {
+		*isPreviousStepRepeated = false
+
+		return
+	}
+
+	if currentRune == backslash && previousRune == backslash && !unicode.IsDigit(nextRune) && !*isPreviousStepRepeated {
+		b.WriteRune(currentRune)
+		*isPreviousStepRepeated = false
+
+		return
+	}
+
+	if unicode.IsDigit(currentRune) && previousRune == backslash &&
+		!unicode.IsDigit(nextRune) && !*isPreviousStepRepeated {
+		b.WriteRune(currentRune)
+		*isPreviousStepRepeated = false
+
+		return
+	}
+
+	if !unicode.IsDigit(nextRune) && !unicode.IsDigit(currentRune) {
+		b.WriteRune(currentRune)
+		*isPreviousStepRepeated = false
+
+		return
+	}
+
+	if unicode.IsDigit(nextRune) {
+		runeRepeatCount, _ := strconv.Atoi(string(nextRune))
+		if runeRepeatCount > 0 {
+			repeatedString := strings.Repeat(string(currentRune), runeRepeatCount)
+			b.WriteString(repeatedString)
+		}
+		*isPreviousStepRepeated = true
+
+		return
+	}
+}
+
+func isDataValid(dataRunes []rune) (bool, error) {
+	dataIsValid, err := isFirstRuneValid(dataRunes[0])
+	if !dataIsValid {
+		return false, err
+	}
+
+	dataIsValid, err = isRuneAfterBackslashValid(dataRunes)
+	if !dataIsValid {
+		return false, err
+	}
+
+	dataIsValid, err = isDigitSequenceValid(dataRunes)
+	if !dataIsValid {
+		return false, err
+	}
+
+	dataIsValid, err = isBackslashSequenceValid(dataRunes)
+	if !dataIsValid {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func isFirstRuneValid(firstRune rune) (bool, error) {
+	if unicode.IsDigit(firstRune) {
+		return false, ErrInvalidStringDigitFirst
+	}
+
+	return true, nil
+}
+
+func isRuneAfterBackslashValid(dataRunes []rune) (bool, error) {
+	for i := 0; i < len(dataRunes)-1; i++ {
+		if dataRunes[i] == backslash && !unicode.IsDigit(dataRunes[i+1]) && dataRunes[i+1] != backslash {
+			return false, ErrInvalidStringBackslashSequence
+		}
+	}
+	if dataRunes[len(dataRunes)-1] == backslash {
+		return false, ErrInvalidStringBackslashSequence
+	}
+
+	return true, nil
+}
+
+func isDigitSequenceValid(dataRunes []rune) (bool, error) {
+	for i := 1; i < len(dataRunes)-1; i++ {
+		if dataRunes[i-1] != backslash && unicode.IsDigit(dataRunes[i]) && unicode.IsDigit(dataRunes[i+1]) {
+			return false, ErrInvalidStringDigitInRawCount
+		}
+
+		if i-2 >= 0 {
+			if dataRunes[i-2] == backslash && dataRunes[i-1] == backslash &&
+				unicode.IsDigit(dataRunes[i]) && unicode.IsDigit(dataRunes[i+1]) {
+				return false, ErrInvalidStringDigitInRawCount
+			}
+		}
+	}
+
+	return true, nil
+}
+
+func isBackslashSequenceValid(dataRunes []rune) (bool, error) {
+	for i := 1; i < len(dataRunes)-2; i++ {
+		if dataRunes[i-1] == backslash && dataRunes[i] == backslash && dataRunes[i+1] == backslash {
+			return false, ErrInvalidStringBackslashInRawCount
+		}
+	}
+
+	return true, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,17 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "\t3", expected: "\t\t\t"},
+		{input: "日本3語", expected: "日本本本語"},
+		{input: "t0", expected: ""},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `\3`, expected: `3`},
+		{input: `\\3`, expected: `\\\`},
+		{input: `\\0`, expected: ``},
 	}
 
 	for _, tc := range tests {
@@ -33,13 +39,46 @@ func TestUnpack(t *testing.T) {
 	}
 }
 
-func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+func TestUnpackInvalidStringFirstDigit(t *testing.T) {
+	invalidStrings := []string{"3abc", "45", "0"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+			require.Truef(t, errors.Is(err, ErrInvalidStringDigitFirst), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackInvalidStringDigitsInRaw(t *testing.T) {
+	invalidStrings := []string{"we45", "aaa\n10b", "asd日213", `\\31`}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidStringDigitInRawCount), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackInvalidStringBackslashes(t *testing.T) {
+	invalidStrings := []string{`qw\ne`, `\qwne`, `qwne\`, `qwe\4\5\`}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidStringBackslashSequence), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackInvalidStringBackslashesInRowCount(t *testing.T) {
+	invalidStrings := []string{`qwe\\\3`, `\\\\8wne`}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidStringBackslashInRawCount), "actual error %q", err)
 		})
 	}
 }
