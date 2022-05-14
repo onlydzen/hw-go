30bfbaca7741987730b1b01bd4eb4328844f80a4
onlydzen <dzen898@yandex.ru>
2022-05-14 03:25:30 +0300

    HW4 is completed

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -49,8 +49,52 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("purge logic by size", func(t *testing.T) {
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ddd", 400)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
+	})
+
+	t.Run("purge logic by usage", func(t *testing.T) {
+		c := NewCache(3)
+
+		// set cache full
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		// last used ccc
+		c.Get("aaa")
+		c.Set("bbb", 2000)
+		c.Get("ccc")
+		c.Set("aaa", 1000)
+		c.Get("bbb")
+
+		// add new item
+		c.Set("ddd", 400)
+
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
 	})
 }
 
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,94 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem
+	tail *ListItem
+	len  int
+}
+
+func (l list) Len() int {
+	return l.len
+}
+
+func (l list) Front() *ListItem {
+	return l.head
+}
+
+func (l list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	l.len++
+
+	if l.head == nil {
+		return setFirstItem(l, v)
+	}
+
+	li := &ListItem{v, l.head, nil}
+	l.head.Prev = li
+	l.head = li
+
+	return li
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	l.len++
+
+	if l.tail == nil {
+		return setFirstItem(l, v)
+	}
+
+	li := &ListItem{v, nil, l.tail}
+	l.tail.Next = li
+	l.tail = li
+
+	return li
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+
+	if l.tail == i {
+		l.tail = i.Prev
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = l.head
+	l.head.Prev = i
+
+	l.head = i
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func setFirstItem(l *list, v interface{}) *ListItem {
+	li := &ListItem{v, nil, nil}
+	l.head = li
+	l.tail = li
+
+	return li
 }
